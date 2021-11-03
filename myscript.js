a3ef0a727fa3a0b83ea12cef0585e527c3b0cbd8
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2021-11-03 19:01:40 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,14 @@
 const HISTORY_SIZE = 1024;
 
 const FREQ_BAR_CENTER_SCALE = 0.1;
+const FREQ_LIMIT = 1024;
 
-const BACKGROUD_FILLSTYLE = 'rgb(0, 0, 0)';
+const TARGET_FREQ = 187;
+const TARGET_COLOR = 'rgb(234, 121, 200)';
+const TARGET_RANGE_SCALE_LOW = 0.2;
+const TARGET_RANGE_SCALE_HIGH = 0.3;
+
+const BACKGROUD_STYLE = 'rgb(0, 0, 0)';
 const RAW_FREQ_FILLSTYLE = 'rgb(200, 200, 200)';
 
 async function onButtonClick(){
@@ -16,15 +22,19 @@ async function onButtonClick(){
     const source = audioCtx.createMediaStreamSource(stream);
     // REF https://developer.mozilla.org/ja/docs/Web/API/AudioContext/createMediaStreamSource
 
-    const sampleRate = audioCtx.sampleRate;
-    console.log(sampleRate);
-    // REF https://developer.mozilla.org/ja/docs/Web/API/BaseAudioContext/sampleRate
-
     const analyserNode = audioCtx.createAnalyser();
+    analyserNode.fftSize = 32768;
+
     source.connect(analyserNode);
 
-    const bufferLength = analyserNode.frequencyBinCount;
-    console.log(bufferLength);
+    const sampleRate = audioCtx.sampleRate;
+    console.log(`sample rate: ${sampleRate}`);
+    console.log(`frequencyBinCount: ${analyserNode.frequencyBinCount}`);
+    let bufferLength = 0;
+    while (sampleRate/2 / analyserNode.frequencyBinCount * (bufferLength+1) <= FREQ_LIMIT && bufferLength+1 <= analyserNode.frequencyBinCount) {
+        bufferLength++;
+    }
+    const screenFreqLimit = sampleRate/2 / analyserNode.frequencyBinCount * (bufferLength+1);
     const dataArray = new Uint8Array(bufferLength);
 
     const canvas = document.createElement('canvas');
@@ -41,7 +51,11 @@ async function onButtonClick(){
     const freqBarScaleY = canvas.height / bufferLength;
     const history = new Array(HISTORY_SIZE);
 
-    function getPosY(idx) {
+    function getPosYFromFreq(f) {
+        return f / screenFreqLimit * canvas.height;
+    }
+
+    function getPosYFromIdx(idx) {
         return idx * freqBarScaleY;
     }
 
@@ -52,7 +66,7 @@ async function onButtonClick(){
     function drawFreqBar(idx, value, style) {
         const length = getFreqBarLength(value);
         canvasCtx.fillStyle = style;
-        canvasCtx.fillRect(canvas.width * (1 - FREQ_BAR_CENTER_SCALE) - length/2, getPosY(idx), length, freqBarScaleY);
+        canvasCtx.fillRect(canvas.width * (1 - FREQ_BAR_CENTER_SCALE) - length/2, getPosYFromIdx(idx), length, freqBarScaleY);
     }
 
     function draw() {
@@ -63,7 +77,7 @@ async function onButtonClick(){
         analyserNode.getByteFrequencyData(dataArray);
 
         //Draw black background
-        canvasCtx.fillStyle = BACKGROUD_FILLSTYLE;
+        canvasCtx.fillStyle = BACKGROUD_STYLE;
         canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
         //Calc max value
@@ -75,9 +89,25 @@ async function onButtonClick(){
                 maxValue = dataArray[i];
             }
         }
+        console.log(sampleRate/2 / analyserNode.frequencyBinCount * max_i);
 
         history.shift() //TODO optimization optimization **optimization**
         history.push(max_i);
+
+        //Draw target
+        const targetPosY = getPosYFromFreq(TARGET_FREQ);
+        const targetPosYlow = getPosYFromFreq(TARGET_FREQ * (1 - TARGET_RANGE_SCALE_LOW));
+        const targetPosYhigh = getPosYFromFreq(TARGET_FREQ * (1 + TARGET_RANGE_SCALE_HIGH));
+        let gradientLow = canvasCtx.createLinearGradient(0, targetPosYlow, 0, targetPosY);
+        gradientLow.addColorStop(0, BACKGROUD_STYLE);
+        gradientLow.addColorStop(1, TARGET_COLOR);
+        canvasCtx.fillStyle = gradientLow;
+        canvasCtx.fillRect(0, targetPosYlow, canvas.width, targetPosY);
+        let gradientHigh = canvasCtx.createLinearGradient(0, targetPosY, 0, targetPosYhigh);
+        gradientHigh.addColorStop(0, TARGET_COLOR);
+        gradientHigh.addColorStop(1, BACKGROUD_STYLE);
+        canvasCtx.fillStyle = gradientHigh;
+        canvasCtx.fillRect(0, targetPosY, canvas.width, targetPosYhigh);
 
         //Draw spectrum
         for (let i = 0; i < bufferLength; i++) {
@@ -88,8 +118,8 @@ async function onButtonClick(){
         for (let i = 0; i < HISTORY_SIZE - 1; i++) {
             if (history[i] !== undefined && history[i+1] !== undefined) {
                 canvasCtx.beginPath();
-                canvasCtx.moveTo(historyScaleX * i, getPosY(history[i]));
-                canvasCtx.lineTo(historyScaleX * (i+1), getPosY(history[i+1]))
+                canvasCtx.moveTo(historyScaleX * i, getPosYFromIdx(history[i]));
+                canvasCtx.lineTo(historyScaleX * (i+1), getPosYFromIdx(history[i+1]))
                 canvasCtx.strokeStyle = "red";
                 canvasCtx.lineWidth = 1;
                 canvasCtx.stroke();
