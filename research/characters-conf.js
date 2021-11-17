9bda9d8615a69a497675ec975fd6524e0337f002
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2021-11-17 22:55:06 +0900

    Added characters-conf.js

---
+++ research/characters-conf.js
@@ -0,0 +1,23 @@
+const CHARACTERS = 
+{
+    "susie": {
+        "color": [137, 23, 107],
+        "freq_range": [180, 195]
+    },
+    "ralsei": {
+        "color": [79, 201, 137],
+        "freq_range": [220, 235]
+    },
+    "noelle": {
+        "color": [255, 206, 49],
+        "freq_range": [320, 330]
+    },
+    "berdly": {
+        "color": [2, 206, 253],
+        "freq_range": [95, 125]
+    },
+    "queen": {
+        "color": [1, 161, 232],
+        "freq_range": [0, 0]
+    }
+}
